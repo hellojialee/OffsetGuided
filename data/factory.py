Change optimizer and fix the Gradient abnormal issue during training

data/factory.py
@@ -127,6 +127,7 @@ def dataset_factory(args, preprocess, target_transforms):
 
 if __name__ == '__main__':  # for debug
     from time import time
+    import timeit
     import argparse
     import encoder
     import matplotlib.pyplot as plt
@@ -169,6 +170,7 @@ if __name__ == '__main__':  # for debug
             mask_miss = annos[0][-1].numpy().astype(np.float32)  # bool -> float
             bg_hmp = annos[0][1].numpy()
             offset = annos[1][0].numpy()
+            offset[np.isinf(offset)] = 0
 
             # # show the generated ground truth
             if show_image:
@@ -226,28 +228,9 @@ if __name__ == '__main__':  # for debug
                                n_images=1000, shuffle=True)
 
     # test the data generator
-    # print(timeit.timeit(stmt='test_augmentation_speed(val_client, False)',  # True
-    #                     setup="from __main__ import test_augmentation_speed;"
-    #                           "from __main__ import val_client",
-    #                     number=3))  # run for number times  # generate 17 samples per second
-
-    test_val_loader = torch.utils.data.DataLoader(
-        val_client, batch_size=args.batch_size, shuffle=False,
-        pin_memory=args.pin_memory,
-        num_workers=args.loader_workers, drop_last=True)
-
-    t0 = time()
-    count = 0
-    print(torch.cuda.get_device_name(0))
-    torch.backends.cudnn.benchmark = True
-    for epoch in range(20):
-        for bath_id, (images, annos, metas) in enumerate(test_val_loader):
-            annos = [[x.cuda() for x in pack] for pack in annos]
-            img = images[0]  # .cuda(non_blocking=True)  # , non_blocking=True
-            count += len(img)
-            print(bath_id, ' of ', epoch)
-            if count > 200:
-                break
-    print('**************** ', count / (time() - t0))
+    print(timeit.timeit(stmt='test_augmentation_speed(val_client, True)',  # True
+                        setup="from __main__ import test_augmentation_speed;"
+                              "from __main__ import val_client",
+                        number=3))  # run for number times  # generate 17 samples per second
 
 

encoder/heatmap.py
@@ -96,6 +96,7 @@ class HeatMapGenerator(object):
         # get the gaussian peak spread
         self.gaussian_size = 2 * math.ceil(
             (math.sqrt(-self.double_sigma2 * math.log(self.gaussian_clip_thre))) / stride)
+        assert self.gaussian_clip_thre > 0 and self.gaussian_size > 0, 'should bigger than zero'
         # cached common parameters which same for all iterations and all pictures
         self.out_w = self.in_w // stride
         self.out_h = self.in_h // stride
@@ -177,7 +178,7 @@ class HeatMapGenerator(object):
             # exp = np.exp(-dist)
 
             # overlap Gaussian peaks by taking the maximum
-            # must use slice view to overlap original array
+            # Must use slice view to overlap original array!
             patch = heatmaps[slice_y, slice_x, layer]
             mask = exp > patch
             patch[mask] = exp[mask]

models/factory.py
@@ -23,7 +23,7 @@ def net_cli(parser):
     group.add_argument('--no-pretrain', dest='pretrained', default=True,
                        action='store_false',
                        help='create BaseNet without pretraining')
-    group.add_argument('--basenet-checkpoint', default="../weights/hourglass_104_renamed.pth",
+    group.add_argument('--basenet-checkpoint', default="weights/hourglass_104_renamed.pth",
                        type=str, help='Path to the pre-trained model and optimizer.')
     group = parser.add_argument_group('head network configuration')
     group.add_argument('--headnets', default=['hmp', 'omp'], nargs='+',
@@ -42,7 +42,7 @@ def net_cli(parser):
                             'in separate channels')
 
     group = parser.add_argument_group('loss configuration')
-    group.add_argument('--lambdas', default=[1, 1, 0.001, 1],
+    group.add_argument('--lambdas', default=[1, 1, 1, 1],  # 0.001
                        type=float, nargs='+',
                        help='learning task wights, directly multiply, not averaged')
     group.add_argument('--stack-weights', default=[1, 1],
@@ -54,13 +54,24 @@ def net_cli(parser):
     group.add_argument('--offset-loss', default='offset_l1_loss',
                        choices=['offset_l1_loss', 'offset_laplace_loss'],
                        help='loss for offeset regression')
+    group.add_argument('--sqrt-re', default=False, action='store_true',
+                       help='rescale the offset loss using torch.sqrt')
     group.add_argument('--scale-loss', default='scale_l1_loss',
                        choices=['scale_l1_loss'],
                        help='loss for keypoint scale regression')
+    group.add_argument('--ftao', default=losses.TAU, type=float,
+                       help='threshold between fore/background in focal L2 loss during training')
+    group.add_argument('--fgamma', default=losses.GAMMA, type=float,
+                       help='order of scaling factor in focal L2 loss during training')
+    group.add_argument('--lmargin', default=losses.MARGIN, type=float,
+                       help='offset length below this value will not be punished during training')
 
 
 def model_factory(args):
     """Build the whole model from scratch from the args"""
+    losses.TAU = args.ftao
+    losses.GAMMA = args.fgamma
+    losses.MARGIN = args.lmargin
 
     if 'hourglass' in args.basenet:
         # build the base network
@@ -87,7 +98,8 @@ def model_factory(args):
             args.stack_weights,
             args.hmp_loss,
             args.offset_loss,
-            args.scale_loss)
+            args.scale_loss,
+            args.sqrt_re)
 
         model = networks.NetworkWrap(basenet, headnets)
 
@@ -133,7 +145,6 @@ if __name__ == '__main__':
     logging.basicConfig(level=log_level)
 
     args = debug_parse_args()
-    t = LOG.parent
 
     import sys
 

models/losses.py
@@ -4,6 +4,10 @@ import re
 
 LOG = logging.getLogger(__name__)
 
+TAU = 0.01  # threshold between fore/background in focal L2 loss during training
+GAMMA = 1  # order of scaling factor in focal L2 loss during training
+MARGIN = 0.1  # offset length below this value will not be punished
+
 
 def l1(x, t):
     """Compute the L1 loss of two tensors."""
@@ -11,7 +15,7 @@ def l1(x, t):
 
 
 def l2(x, t):
-    out = (x - t) ** 2 / 2
+    out = torch.mul((x - t) ** 2, 0.5)
     return out.sum()
 
 
@@ -20,10 +24,10 @@ def laplace(norm, logb):
     return out.sum()
 
 
-def focal_l2(s, sxing, tau=0.01, gamma=1):
+def focal_l2(s, sxing, tau=TAU, gamma=GAMMA):
     st = torch.where(torch.ge(sxing, tau), s, 1. - s)
     factor = torch.abs(1. - st) ** gamma
-    out = (s - sxing) ** 2 * factor / 2
+    out = torch.mul((s - sxing) ** 2 * factor, 0.5)
     return out.sum()
 
 
@@ -131,12 +135,11 @@ class HeatMapsLoss(object):
 
         for stack_i, (hmp, bg_hmp) in enumerate(zip(hmps, bg_hmps)):  # loop each stack
 
-            weighted_loss = self.hmp_loss(hmp, gt_hpm, mask_miss) * self.stack_weights[stack_i]
+            weighted_loss = torch.mul(self.hmp_loss(hmp, gt_hpm, mask_miss), self.stack_weights[stack_i])
             out1.append(weighted_loss)
 
             if len(bg_hmp) > 0:  # background heatmap loss
-                weighted_bgloss = self.hmp_loss(bg_hmp, gt_bghmp, mask_miss) \
-                                  * self.stack_weights[stack_i]
+                weighted_bgloss = torch.mul(self.hmp_loss(bg_hmp, gt_bghmp, mask_miss), self.stack_weights[stack_i])
                 out2.append(weighted_bgloss)
         LOG.debug('hmp loss: %s, \t background hmp loss: %s', out1, out2)
         return sum(out1) / batch_size, sum(out2) / batch_size
@@ -144,7 +147,7 @@ class HeatMapsLoss(object):
 
 class OffsetMapsLoss(object):
 
-    def __init__(self, head_name, n_stacks, stack_weights, off_loss, s_loss):
+    def __init__(self, head_name, n_stacks, stack_weights, off_loss, s_loss, sqrt_re=False):
         super(OffsetMapsLoss, self).__init__()
         assert len(stack_weights) >= n_stacks, type(stack_weights)
 
@@ -153,6 +156,7 @@ class OffsetMapsLoss(object):
         self.stack_weights = [weight / sum(stack_weights) for weight in stack_weights]
         self.off_loss = off_loss
         self.s_loss = s_loss
+        self.sqrt_re = sqrt_re  # resize the offset loss by log
 
         LOG.debug('%s loss config: n_stacks = %d, stack_weights = %s, losses = %s, %s, ',
                   head_name, n_stacks, stack_weights,
@@ -170,17 +174,24 @@ class OffsetMapsLoss(object):
 
         for stack_i, (pred_off, pred_spread, pred_s) in enumerate(
                 zip(pred_off_stacks, pred_spread_stacks, pred_scale_stacks)):
+            inter1 = torch.mul(
+                self.off_loss(pred_off, gt_off, pred_spread, mask_miss),
+                self.stack_weights[stack_i])
+            if self.sqrt_re:
+                inter1 = torch.sqrt(inter1 + MARGIN)  # fixme
+            out1.append(inter1)
 
-            out1.append(self.off_loss(pred_off, gt_off, pred_spread, mask_miss)
-                        * self.stack_weights[stack_i])
             if len(pred_s) > 0:
-                out2.append(self.s_loss(pred_s, gt_s, mask_miss)
-                            * self.stack_weights[stack_i])
+                inter2 = torch.mul(
+                    self.s_loss(pred_s, gt_s, mask_miss),
+                    self.stack_weights[stack_i])
+                out2.append(inter2)
         LOG.debug('connection offset loss: %s, \t keypoint scale loss: %s', out1, out2)
         return sum(out1) / batch_size, sum(out2) / batch_size
 
 
-def lossfuncs_factory(headnames, n_stacks, stack_weights, heatmap_loss, offset_loss, scale_loss):
+def lossfuncs_factory(headnames, n_stacks, stack_weights,
+                      heatmap_loss, offset_loss, scale_loss, sqrt_re):
     """Build loss networks.
 
     Args:
@@ -193,12 +204,13 @@ def lossfuncs_factory(headnames, n_stacks, stack_weights, heatmap_loss, offset_l
     off_loss = getattr(LossChoice, offset_loss)
     s_loss = getattr(LossChoice, scale_loss)
 
-    lossnets = [factory_loss(h, n_stacks, stack_weights, hmp_loss, off_loss, s_loss) for h in headnames]
+    lossnets = [factory_loss(h, n_stacks, stack_weights, hmp_loss, off_loss, s_loss, sqrt_re)
+                for h in headnames]
 
     return lossnets
 
 
-def factory_loss(head_name, n_stacks, stack_weights, hmp_loss, off_loss, s_loss):
+def factory_loss(head_name, n_stacks, stack_weights, hmp_loss, off_loss, s_loss, sqrt_re):
     """
     Build a head network.
 
@@ -218,7 +230,7 @@ def factory_loss(head_name, n_stacks, stack_weights, hmp_loss, off_loss, s_loss)
                      'offsets') or \
             re.match('omp[s]?([0-9]+)$', head_name) is not None:
         LOG.info('select loss net for %s head', head_name)
-        return OffsetMapsLoss(head_name, n_stacks, stack_weights, off_loss, s_loss)
+        return OffsetMapsLoss(head_name, n_stacks, stack_weights, off_loss, s_loss, sqrt_re)
         # 构造并返回Paf，用于生成ground truth paf
     raise Exception('unknown head to create a lossnet: {}'.format(head_name))
 

models/networks.py
@@ -1,6 +1,7 @@
 import logging
 
 import os
+import sys
 import torch
 import torch.nn as nn
 from models import Hourglass104, Hourglass4Stage
@@ -29,7 +30,7 @@ def load_model(model, ckpt_path, optimizer=None, drop_layers=True,
         print("############# No pre-trained parameters are loaded! #############\n"
               "######## Please make sure you initialize the model randomly! #####")
         # return without loading
-        return model, None, start_epoch, start_loss
+        return model, optimizer, start_epoch, start_loss
 
     checkpoint = torch.load(ckpt_path, map_location=torch.device('cpu'))
     LOG.info('Loading pre-trained model %s, checkpoint at epoch %d', ckpt_path,
@@ -55,30 +56,34 @@ def load_model(model, ckpt_path, optimizer=None, drop_layers=True,
     model_state_dict = model.state_dict()  # newly built model
 
     # check loaded parameters and created model parameters
-    msg = 'If you see this, your model does not fully load the ' + \
-          'pre-trained weight. Please make sure ' + \
-          'you have correctly specified the output or regression layers.'
+    msg1 = 'If you see this, your model does not fully load the ' + \
+           'pre-trained weight. Please make sure ' + \
+           'you have correctly built the model layers or the weight shapes.'
+    msg2 = 'If you see this, your model has more parameters than the ' + \
+           'pre-trained weight. Please make sure ' + \
+           'you have correctly specified more layers.'
     for k in state_dict:
         if k in model_state_dict:
             if state_dict[k].shape != model_state_dict[k].shape:
                 LOG.debug(
                     'Skip loading pre-trained parameter %s, current model '
                     'required shape %s, loaded shape %s. %s',
-                    k, model_state_dict[k].shape, state_dict[k].shape, msg)
-                state_dict[k] = model_state_dict[k]  # fix bad params
+                    k, model_state_dict[k].shape, state_dict[k].shape, msg1)
+                state_dict[k] = model_state_dict[k]  # fix badly mismatched params
         else:
             LOG.debug('Drop pre-trained parameter %s which current model dose '
-                      'not have. %s', k, msg)
+                      'not have. %s', k, msg1)
     for k in model_state_dict:
         if not (k in state_dict):
-            LOG.debug('No param in pre-trained model %s. %s', k, msg)
-            state_dict[k] = model_state_dict[k]  # append missing params
+            LOG.debug('No param %s in pre-trained model. %s', k, msg2)
+            state_dict[k] = model_state_dict[k]  # append missing params to rescue
     model.load_state_dict(state_dict, strict=False)
     print(f'Network {model.__class__.__name__} weights have been resumed from checkpoint: {ckpt_path}')
 
     # resume optimizer parameters
     if optimizer is not None and resume_optimizer:
         if 'optimizer_state_dict' in checkpoint:
+            LOG.debug('Resume the optimizer.')
             optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 
             # Here, we must convert the resumed state data of optimizer to gpu.
@@ -86,6 +91,7 @@ def load_model(model, ckpt_path, optimizer=None, drop_layers=True,
             # In the training process, we need cuda version of state tensors,
             # so we have to convert them to gpu.
             if torch.cuda.is_available() and optimizer2cuda:
+                LOG.debug('Remove the optimizer states into GPU.')
                 for state in optimizer.state.values():
                     for k, v in state.items():
                         if torch.is_tensor(v):
@@ -94,13 +100,11 @@ def load_model(model, ckpt_path, optimizer=None, drop_layers=True,
             # param_group['lr'] will be instead set in a separate fun: adjust_learning_rate()
             print('Optimizer {} has been resumed from the checkpoint at epoch {}.'
                   .format(optimizer.__class__.__name__, start_epoch - 1))
+        elif optimizer is not None:
+            print('Optimizer {} is NOT resumed, although the checkpoint exists.'.format(optimizer.__class__.__name__))
         else:
-            print('Optimizer {} has NO pre-trained weights in current checkpoint.'.format(optimizer.__class__.__name__))
-
-    if optimizer is not None and resume_optimizer:
-        return model, optimizer, start_epoch, start_loss
-    else:
-        return model, None, start_epoch, start_loss
+            print('Optimizer is {}.'.format(optimizer))
+    return model, optimizer, start_epoch, start_loss
 
 
 def save_model(path, epoch, train_loss, model, optimizer=None):
@@ -151,12 +155,14 @@ def initialize_weights(model):
 
 class NetworkWrap(torch.nn.Module):
     """Wrap the basenet and headnets into a single module."""
+
     def __init__(self, basenet, headnets):
         super(NetworkWrap, self).__init__()
         self.basenet = basenet
         # Notice!  subnets in list or dict must be warped
         # by ModuleList to register trainable params
         self.headnets = torch.nn.ModuleList(headnets)
+        LOG.debug('warp the basnet and headnets into a whole model')
 
     def forward(self, img_tensor):
         # Batch will be divided and Parallel Model
@@ -188,6 +194,7 @@ def basenet_factory(basenet_name):
             nstack = 4  # stacked number of hourglass
             hourglass_inp_dim = 256
             increase = 128  # increased channels once down-sampling through networks
+
         net_opt = IMHNOpt()
         out_dim = 50
-        raise Exception('Unknown base network in {}'.format(basenet_name))
+        raise Exception('unknown base network in {}'.format(basenet_name))

train_distributed.py
@@ -43,8 +43,13 @@ def train_cli():
 
     parser.add_argument('--resume', '-r', action='store_true', default=False,
                         help='resume from checkpoint')
+    parser.add_argument('--drop-optim-state', dest='resume_optimizer', action='store_false',
+                        default=True,
+                        help='do not resume the optimizer from checkpoint.')
     parser.add_argument('--freeze', action='store_true', default=False,
                         help='freeze the pre-trained layers of the BaseNet, i.e. Backbone')
+    parser.add_argument('--drop-layers', action='store_true', default=False,
+                        help='drop some layers described in models.networks.load_model')
     parser.add_argument('--epochs', default=100, type=int, metavar='N',
                         help='number of epochs to train')
     parser.add_argument('--warmup', action='store_true', default=False,
@@ -52,7 +57,7 @@ def train_cli():
     parser.add_argument('--checkpoint-path', '-p',
                         default='link2checkpoints_storage',
                         help='folder path checkpoint storage of the whole pose estimation model')
-    parser.add_argument('--max-grad_norm', default=10, type=float,
+    parser.add_argument('--max-grad-norm', default=float('inf'), type=float,  # not implemented yet
                         help=(
                             "If the norm of the gradient vector exceeds this, "
                             "re-normalize it to have the norm equal to max_grad_norm"))
@@ -71,15 +76,15 @@ def train_cli():
                        help='print frequency (default: 10)')
 
     group = parser.add_argument_group('optimizer configuration')
-    group.add_argument('--optimizer', type=str, default='sgd',
+    group.add_argument('--optimizer', type=str, default='adam',
                        choices=['sgd', 'adam'])
-    group.add_argument('--learning-rate', type=float, default=2.5e-5,
+    group.add_argument('--learning-rate', type=float, default=2.5e-4,
                        metavar='LR',
                        help='learning rate')
     group.add_argument('--momentum', default=0.9, type=float, metavar='M',
-                       help='momentum')
-    group.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
-                       metavar='W', help='weight decay (default: 1e-4)')
+                       help='momentum for sgd')
+    group.add_argument('--weight-decay', '--wd', default=0, type=float,
+                       metavar='W', help='weight decay (e.g. 1e-4)')
 
     args = parser.parse_args()
 
@@ -102,19 +107,21 @@ def main():
     print(f"CUDNN VERSION: {torch.backends.cudnn.version()}\n")
 
     if args.local_rank == 0:
-
         # build epoch recorder
-        os.makedirs(args.checkpoint_path, exist_ok=True)
-        recorder = open(os.path.join('./' + args.checkpoint_path, 'log'), 'a+')
-        recorder.write('\ncmd line: {} \n \targs dict: {}'.format(' '.join(sys.argv), vars(args)))
-        recorder.flush()
-        recorder.close()
+        # os.makedirs(args.checkpoint_path, exist_ok=True)
+        with open(os.path.join(args.checkpoint_path, 'log'), 'a+') as f:
+            f.write('\n\ncmd line: {} \n \targs dict: {}'.format(' '.join(sys.argv), vars(args)))
+            f.flush()
 
     torch.backends.cudnn.benchmark = True
     # print(vars(args))
     args.pin_memory = False
+    args.use_cuda = False
     if torch.cuda.is_available():
+        args.use_cuda = True
         args.pin_memory = True
+    else:
+        raise ValueError(f'CUDA is available: {args.use_cuda}')
 
     args.distributed = False
     if 'WORLD_SIZE' in os.environ:
@@ -183,13 +190,26 @@ def main():
             if 'basenet' in name:
                 param.requires_grad = False
 
-    # optimizer = apex_optim.FusedSGD(filter(lambda p: p.requires_grad, model.parameters()),
-    #                                 lr=opt.learning_rate * args.world_size, momentum=0.9, weight_decay=5e-4)
-    optimizer = torch.optim.SGD(
-        filter(lambda p: p.requires_grad, model.parameters()),
-        lr=args.learning_rate * args.world_size, momentum=args.momentum,
-        weight_decay=args.weight_decay)
-    # optimizer = apex_optim.FusedAdam(model.parameters(), lr=opt.learning_rate * args.world_size, weight_decay=1e-4)
+    if args.optimizer == 'sgd':
+        # optimizer = apex_optim.FusedSGD(
+        # filter(lambda p: p.requires_grad, model.parameters()),
+        # lr=opt.learning_rate * args.world_size, momentum=0.9, weight_decay=5e-4)
+        optimizer = torch.optim.SGD(
+            filter(lambda p: p.requires_grad, model.parameters()),
+            lr=args.learning_rate * args.world_size, momentum=args.momentum,
+            weight_decay=args.weight_decay)
+    elif args.optimizer == 'adam':
+        optimizer = apex_optim.FusedAdam(
+            filter(lambda p: p.requires_grad, model.parameters()),
+            lr=args.learning_rate * args.world_size,
+            weight_decay=args.weight_decay)
+    else:
+        raise Exception(f'optimizer {args.optimizer} is not supported')
+
+    if args.resume:
+        model, optimizer, start_epoch, best_loss = models.networks.load_model(
+            model, args.checkpoint_whole, optimizer, resume_optimizer=args.resume_optimizer,
+            drop_layers=args.drop_layers, optimizer2cuda=args.use_cuda)
 
     # Initialize Amp.  Amp accepts either values or strings for the optional override arguments,
     # for convenient interoperation with argparse.
@@ -199,11 +219,6 @@ def main():
                                       loss_scale=args.loss_scale)  # Dynamic loss scaling is used by default.
     if args.distributed:
         model = DDP(model, delay_allreduce=True)
-
-    if args.resume:
-        model, optimizer, start_epoch, best_loss = models.networks.load_model(
-            model, args.checkpoint_whole, optimizer, resume_optimizer=True,
-            drop_layers=False, optimizer2cuda=True)
 
     train_sampler = None
     val_sampler = None
@@ -243,12 +258,13 @@ def train(train_loader, train_sampler, model, criterion, optimizer, epoch):
     # disturb and allocation data differently at each epcoh
     # train_sampler make each GPU process see 1/(world_size) training samples per epoch
     if args.distributed:
+        #  calling the set_epoch method is needed to make shuffling work
         train_sampler.set_epoch(epoch)
 
     # adjust_learning_rate_cyclic(optimizer, epoch, start_epoch)  # start_epoch
     print(
-        '\nLearning rate at this epoch is: %0.9f' % optimizer.param_groups[0][
-            'lr'])  # scheduler.get_lr()[0]
+        '\nLearning rate at this epoch is: %0.9f\n' % optimizer.param_groups[0][
+            'lr'])
 
     batch_time = AverageMeter()
     losses = AverageMeter()
@@ -258,7 +274,7 @@ def train(train_loader, train_sampler, model, criterion, optimizer, epoch):
         # # ##############  Use fun of 'adjust learning rate' #####################
         adjust_learning_rate(optimizer, epoch, batch_idx, len(train_loader),
                              use_warmup=args.warmup)
-        # print('\nLearning rate at this epoch is: %0.9f\n' % optimizer.param_groups[0]['lr'])  # scheduler.get_lr()[0]
+        LOG.debug('\nLearning rate at this batch is: %0.9f\n', optimizer.param_groups[0]['lr'])
         # # ##########################################################
 
         #  这允许异步 GPU 复制数据也就是说计算和数据传输可以同时进.
@@ -273,14 +289,16 @@ def train(train_loader, train_sampler, model, criterion, optimizer, epoch):
         multi_losses = []
         for out, lossfun, anno in zip(outputs, criterion, anno_heads):
             multi_losses += list(lossfun(out, *anno))
-        # weight the multi-task losses
-        weighted_losses = [lam * l for lam, l in
+        # weight the multi-task losses, args.lambdas defined in models.factory
+        assert len(multi_losses) <= len(args.lambdas), 'lambdas is incomplete'
+        weighted_losses = [torch.mul(lam, l) for lam, l in
                            zip(args.lambdas, multi_losses)]
-        loss = sum(weighted_losses)  # args.lambdas defined in models.factory
+        loss = sum(weighted_losses)  # type: torch.Tensor
 
         if loss.item() > 1e8:  # try to rescue the gradient explosion
-            print("\nOh My God ! \nLoss is abnormal, drop this batch !")
-            continue
+            import warnings
+            warnings.warn("\nOh My God! \nLoss is abnormal, drop this batch!")
+            loss.zero_()
 
         LOG.info({
             'type': f'train-at-rank{args.local_rank}',
@@ -294,7 +312,9 @@ def train(train_loader, train_sampler, model, criterion, optimizer, epoch):
         with amp.scale_loss(loss, optimizer) as scaled_loss:
             scaled_loss.backward()
 
-        # torch.nn.utils.clip_grad_norm_(amp.master_params(optimizer), args.max_grad_norm)  # todo
+        # According to our experience, clip norm is easy to destroy the training after few epochs
+        # torch.nn.utils.clip_grad_norm_(amp.master_params(optimizer), args.max_grad_norm)
+
         optimizer.step()
 
         if batch_idx % args.print_freq == 0:
@@ -328,12 +348,10 @@ def train(train_loader, train_sampler, model, criterion, optimizer, epoch):
     #
     if args.local_rank == 0:
         # Write the log file each epoch.
-        os.makedirs(args.checkpoint_path, exist_ok=True)
-        recorder = open(os.path.join('./' + args.checkpoint_path, 'log'), 'a+')
-        recorder.write('\nEpoch {}\ttrain_loss: {}'.format(epoch,
-                                                           losses.avg))  # validation时不要\n换行
-        recorder.flush()
-        recorder.close()
+        with open(os.path.join('./' + args.checkpoint_path, 'log'), 'a+') as f:
+            # validation时写字符串后不要\n换行
+            f.write('\nEpoch {}\ttrain_loss: {}'.format(epoch, losses.avg))
+            f.flush()
 
         if losses.avg < float('inf'):  # < best_loss
             # Update the best_loss if the average loss drops
@@ -367,7 +385,7 @@ def test(val_loader, val_sampler, model, criterion, optimizer, epoch):
             for out, lossfun, anno in zip(outputs, criterion, anno_heads):
                 multi_losses += list(lossfun(out, *anno))
             # weight the multi-task losses
-            weighted_losses = [lam * l for lam, l in
+            weighted_losses = [torch.mul(lam, l) for lam, l in
                                zip(args.lambdas, multi_losses)]
             loss = sum(weighted_losses)  # args.lambdas defined in models.factory
 
@@ -375,6 +393,7 @@ def test(val_loader, val_sampler, model, criterion, optimizer, epoch):
             'type': f'validate-at-rank{args.local_rank}',
             'epoch': epoch,
             'batch': batch_idx,
+            # 'metas': metas,
             'head_losses': [round(to_python_float(l.detach()) if torch.is_tensor(l) else l, 6)
                             for l in multi_losses],
             'loss': round(to_python_float(loss.detach()), 6),
@@ -406,11 +425,9 @@ def test(val_loader, val_sampler, model, criterion, optimizer, epoch):
 
     if args.local_rank == 0:  # Print them in the Process 0
         # Write the log file each epoch.
-        os.makedirs(args.checkpoint_path, exist_ok=True)
-        logger = open(os.path.join('./' + args.checkpoint_path, 'log'), 'a+')
-        logger.write('\tval_loss: {}'.format(losses.avg))  # validation时不要\n换行
-        logger.flush()
-        logger.close()
+        with open(os.path.join('./' + args.checkpoint_path, 'log'), 'a+') as f:
+            f.write('\tval_loss: {}'.format(losses.avg))  # validation时不要\n换行
+            f.flush()
 
 
 def adjust_learning_rate(optimizer, epoch, step, len_epoch, use_warmup=False):
@@ -421,12 +438,12 @@ def adjust_learning_rate(optimizer, epoch, step, len_epoch, use_warmup=False):
 
     lr = args.learning_rate * args.world_size * (0.2 ** factor)
 
-    """Warmup"""
+    """Warmup the learning rate"""
     if use_warmup:
-        if epoch < 5:
+        if epoch < 12:
             # print('=============>  Using warm-up learning rate....')
             lr = lr * float(1 + step + epoch * len_epoch) / (
-                    5. * len_epoch)  # len_epoch=len(train_loader)
+                    12. * len_epoch)  # len_epoch=len(train_loader)
 
     # if(args.local_rank == 0):
     #     print("epoch = {}, step = {}, lr = {}".format(epoch, step, lr))