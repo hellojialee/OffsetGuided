Fix loss computation; Add draw_limb_offset

data/factory.py
@@ -176,12 +176,14 @@ if __name__ == '__main__':  # for debug
             if show_image:
                 image = image.transpose((1, 2, 0))
                 show_labels = cv2.resize(bg_hmp.transpose((1, 2, 0)), image.shape[:2], interpolation=cv2.INTER_CUBIC)
-                # offsets = cv2.resize(offsets.transpose((1, 2, 0)), image.shape[:2], interpolation=cv2.INTER_NEAREST)
+                offset = cv2.resize(offset.transpose((1, 2, 0)), image.shape[:2], interpolation=cv2.INTER_NEAREST)
                 mask_miss = np.repeat(mask_miss.transpose((1, 2, 0)), 3, axis=2)
                 mask_miss = cv2.resize(mask_miss, image.shape[:2], interpolation=cv2.INTER_NEAREST)
-                # image = cv2.resize(image, mask_miss.shape[:2], interpolation=cv2.INTER_NEAREST)
-                plt.imshow(image)  # We manually set Opencv earlier: RGB
-                # plt.imshow(mask_miss, alpha=0.5)  # mask_all
+                plt.imshow(image)
+                plt.imshow(mask_miss, alpha=0.1)  # mask_all
+                plt.show()
+
+                plt.imshow(image)  # We have manually set Opencv earlier: RGB
                 plt.imshow(show_labels, alpha=0.5)  # mask_all
                 plt.show()
         print("produce %d samples per second: " % (batch / (time() - start)))  # about 70~80 FPS on MBP-13

demo_image.py
@@ -5,6 +5,7 @@ import cv2
 import matplotlib.pyplot as plt
 import time
 import datetime
+import numpy as np
 
 import torch
 import torch.distributed as dist
@@ -15,6 +16,7 @@ import transforms
 import encoder
 import models
 import logs
+from visualization.show import draw_limb_offset
 
 try:
     from apex.parallel import DistributedDataParallel as DDP
@@ -48,6 +50,8 @@ def demo_cli():
     parser.add_argument('--checkpoint-path', '-p',
                         default='link2checkpoints_storage',
                         help='folder path checkpoint storage of the whole pose estimation model')
+    parser.add_argument('--show-limb-idx', default=None, type=int, metavar='N',
+                        help='draw the vector of limb connection offset and connected keypoints')
 
     group = parser.add_argument_group('apex configuration')
     group.add_argument("--local_rank", default=0, type=int)
@@ -59,9 +63,9 @@ def demo_cli():
     group.add_argument('--print-freq', '-f', default=10, type=int, metavar='N',
                        help='print frequency (default: 10)')
 
-    args = parser.parse_args(
-        '--checkpoint-whole link2checkpoints_storage/PoseNet_3_epoch.pth --resume --no-pretrain'.split())
-
+    # args = parser.parse_args(
+    #     '--checkpoint-whole link2checkpoints_storage/PoseNet_18_epoch.pth --resume --no-pretrain'.split())
+    args = parser.parse_args()
     return args
 
 
@@ -121,7 +125,6 @@ def main():
                            keep_batchnorm_fp32=args.keep_batchnorm_fp32,
                            loss_scale=args.loss_scale)
 
-
     # 创建数据加载器，在训练和验证步骤中喂数据
     train_loader = torch.utils.data.DataLoader(train_data,
                                                batch_size=args.batch_size,
@@ -138,15 +141,23 @@ def main():
 
     # ############################# Train and Validate #############################
     for epoch in range(start_epoch, start_epoch + args.epochs):
-        test(train_loader, model, lossfuns, epoch)
+        test(val_loader, model, lossfuns, epoch, show_limb_idx=args.show_limb_idx)
 
 
-def test(val_loader, model, criterion, epoch, show_image=True):
+def test(val_loader, model, criterion, epoch, show_limb_idx=None, s=5):
+    """
+    Args:
+        val_loader:
+        model:
+        criterion:
+        epoch:
+        show_limb_idx (int): show the limb connection offset vector with this index
+        s (int): control the sparsity of the vector arrows
+    """
     print('\n ############################# Test phase, Epoch: {} #############################'.format(epoch))
     model.eval()
-    # DistributedSampler 中记录目前的 epoch 数， 因为采样器是根据 epoch 来决定如何打乱分配数据进各个进程
     # if args.distributed:
-    #     val_sampler.set_epoch(epoch)  # 验证集太小，不够4个划分
+    #     val_sampler.set_epoch(epoch)
     batch_time = AverageMeter()
     losses = AverageMeter()
     end = time.time()
@@ -176,21 +187,17 @@ def test(val_loader, model, criterion, epoch, show_image=True):
             'loss': round(to_python_float(loss.detach()), 6),
         })
 
-        if show_image:
-            import numpy as np
+        if show_limb_idx:
             hmps = outputs[0][0][1].cpu().numpy()
             offs = outputs[1][0][1].cpu().numpy()
             offs[np.isinf(offs)] = 0
+            hmp = hmps[0]
+            off = offs[0]
             image = images.cpu().numpy()[0, ...].transpose((1, 2, 0))  # the first image
-            show_labels = cv2.resize(hmps[0].transpose((1, 2, 0)), image.shape[:2], interpolation=cv2.INTER_CUBIC)
-            # offsets = cv2.resize(offsets.transpose((1, 2, 0)), image.shape[:2], interpolation=cv2.INTER_NEAREST)
-            # mask_miss = np.repeat(mask_miss.transpose((1, 2, 0)), 3, axis=2)
-            # mask_miss = cv2.resize(mask_miss, image.shape[:2], interpolation=cv2.INTER_NEAREST)
-            # image = cv2.resize(image, mask_miss.shape[:2], interpolation=cv2.INTER_NEAREST)
-            plt.imshow(image)  # We manually set Opencv earlier: RGB
-            # plt.imshow(mask_miss, alpha=0.5)  # mask_all
-            plt.imshow(show_labels[:, :, 15], alpha=0.5)  # mask_all
-            plt.show()
+
+            skeleton = encoder.OffsetMaps.skeleton
+
+            draw_limb_offset(hmp, image, off, s, show_limb_idx, skeleton)
 
         if batch_idx % args.print_freq == 0:
             reduced_loss = loss.data

encoder/offset.py
@@ -162,8 +162,8 @@ class OffsetMapGenerator(object):
             slice_x = slice(x_min, x_max + 1)
             slice_y = slice(y_min, y_max + 1)
 
-            offset_x = (joint2[0] - self.grid_x[slice_x].astype(np.float32))
-            # type: np.ndarray # joint2[i, 1] -> y
+            offset_x = (joint2[0] - self.grid_x[slice_x].astype(np.float32))  # type: np.ndarray
+            # joint2[i, 1] -> y
             offset_y = (joint2[1] - self.grid_y[slice_y].astype(np.float32))
             offset_x_mesh = np.repeat(offset_x.reshape(1, -1), offset_y.shape[0], axis=0)
             offset_y_mesh = np.repeat(offset_y.reshape(-1, 1), offset_x.shape[0], axis=1)

models/losses.py
@@ -4,31 +4,36 @@ import re
 
 LOG = logging.getLogger(__name__)
 
+
 TAU = 0.01  # threshold between fore/background in focal L2 loss during training
 GAMMA = 1  # order of scaling factor in focal L2 loss during training
 MARGIN = 0.1  # offset length below this value will not be punished
 
 
 def l1(x, t):
-    """Compute the L1 loss of two tensors."""
-    return torch.abs(x - t).sum()
+    """Compute the element-wise L1 loss of two tensors."""
+    out = torch.abs(x - t)
+    return out
 
 
 def l2(x, t):
+    """Element-wise L2 loss"""
     out = torch.mul((x - t) ** 2, 0.5)
-    return out.sum()
+    return out
 
 
 def laplace(norm, logb):
+    """Element-wise laplace loss"""
     out = 0.693147 + logb + norm * torch.exp(-logb)
-    return out.sum()
+    return out
 
 
 def focal_l2(s, sxing, tau=TAU, gamma=GAMMA):
+    """Element-wise focal l2 loss"""
     st = torch.where(torch.ge(sxing, tau), s, 1. - s)
     factor = torch.abs(1. - st) ** gamma
     out = torch.mul((s - sxing) ** 2 * factor, 0.5)
-    return out.sum()
+    return out
 
 
 def tensor_loss(pred, gt, mask_miss, fun):
@@ -73,6 +78,8 @@ class LossChoice(object):
 
     @staticmethod
     def offset_l1_loss(pred, gt, _, mask_miss):
+        """sqrt_re may be used in lossfun Class to rescale the delta x, y element-wisely,
+        other than based on vetctor length """
         return tensor_loss(pred, gt, mask_miss, l1)
 
     @staticmethod
@@ -134,14 +141,16 @@ class HeatMapsLoss(object):
         out1, out2 = [], []
 
         for stack_i, (hmp, bg_hmp) in enumerate(zip(hmps, bg_hmps)):  # loop each stack
-
-            weighted_loss = torch.mul(self.hmp_loss(hmp, gt_hpm, mask_miss), self.stack_weights[stack_i])
+            inter1 = self.hmp_loss(hmp, gt_hpm, mask_miss)  # type: torch.Tensor
+            weighted_loss = torch.mul(inter1.sum(), self.stack_weights[stack_i])
             out1.append(weighted_loss)
 
             if len(bg_hmp) > 0:  # background heatmap loss
-                weighted_bgloss = torch.mul(self.hmp_loss(bg_hmp, gt_bghmp, mask_miss), self.stack_weights[stack_i])
+                inter2 = self.hmp_loss(bg_hmp, gt_bghmp, mask_miss)  # type: torch.Tensor
+                weighted_bgloss = torch.mul(inter2.sum(), self.stack_weights[stack_i])
                 out2.append(weighted_bgloss)
-        LOG.debug('hmp loss: %s, \t background hmp loss: %s', out1, out2)
+        LOG.debug('hmp loss at each stack: %s, \t background hmp loss at eack stack: %s'
+                  , torch.tensor(out1).cpu().numpy().tolist(), torch.tensor(out2).cpu().numpy().tolist())
         return sum(out1) / batch_size, sum(out2) / batch_size
 
 
@@ -156,7 +165,7 @@ class OffsetMapsLoss(object):
         self.stack_weights = [weight / sum(stack_weights) for weight in stack_weights]
         self.off_loss = off_loss
         self.s_loss = s_loss
-        self.sqrt_re = sqrt_re  # resize the offset loss by log
+        self.sqrt_re = sqrt_re  # resize the offset loss by
 
         LOG.debug('%s loss config: n_stacks = %d, stack_weights = %s, losses = %s, %s, ',
                   head_name, n_stacks, stack_weights,
@@ -174,19 +183,18 @@ class OffsetMapsLoss(object):
 
         for stack_i, (pred_off, pred_spread, pred_s) in enumerate(
                 zip(pred_off_stacks, pred_spread_stacks, pred_scale_stacks)):
-            inter1 = torch.mul(
-                self.off_loss(pred_off, gt_off, pred_spread, mask_miss),
-                self.stack_weights[stack_i])
+            inter1 = self.off_loss(pred_off, gt_off, pred_spread, mask_miss)
             if self.sqrt_re:
-                inter1 = torch.sqrt(inter1 + MARGIN)  # fixme
-            out1.append(inter1)
+                inter1 = torch.sqrt(inter1 + MARGIN)  # type: torch.Tensor # todo: tune the MARGIN?
+            weighted_offloss = torch.mul(inter1.sum(), self.stack_weights[stack_i])
+            out1.append(weighted_offloss)
 
             if len(pred_s) > 0:
-                inter2 = torch.mul(
-                    self.s_loss(pred_s, gt_s, mask_miss),
-                    self.stack_weights[stack_i])
-                out2.append(inter2)
-        LOG.debug('connection offset loss: %s, \t keypoint scale loss: %s', out1, out2)
+                inter2 = self.s_loss(pred_s, gt_s, mask_miss)  # type: torch.Tensor
+                weighted_sloss = torch.mul(inter2.sum(), self.stack_weights[stack_i])
+                out2.append(weighted_sloss)
+        LOG.debug('connection offset loss at each stack: %s, \t keypoint scale loss at each stack: %s'
+                  , torch.tensor(out1).cpu().numpy().tolist(), torch.tensor(out2).cpu().numpy().tolist())
         return sum(out1) / batch_size, sum(out2) / batch_size
 
 
@@ -203,6 +211,7 @@ def lossfuncs_factory(headnames, n_stacks, stack_weights,
 
     off_loss = getattr(LossChoice, offset_loss)
     s_loss = getattr(LossChoice, scale_loss)
+    LOG.debug('tau = %.4f, gamma = %d, margin = %.4f', TAU, GAMMA, MARGIN)
 
     lossnets = [factory_loss(h, n_stacks, stack_weights, hmp_loss, off_loss, s_loss, sqrt_re)
                 for h in headnames]